8e17cd0c124acbea700a9f8c86cb7e380b119165
teraliva <lisaparsentieva@gmail.com>
2022-10-12 18:55:32 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const tildeImporter = require('node-sass-tilde-importer');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const flatten = require('gulp-flatten');
+
+function style() {
+    return src('src/main.scss')
+        .pipe(sass({
+            importer: tildeImporter
+        }))
+        .on('error', sass.logError)
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest('build/css'))
+}
+
+function favicon() {
+    return src('source/favicon/*.*')
+        .pipe(flatten({
+            includeParents: 0
+        }))
+        .pipe(dest('../public/'))
+}
+
+function watching() {
+    watch('src/**/*scss', style);
+}
+
+exports.style = style;
+exports.favicon = favicon;
+exports.watch = watching;
+exports.default = series(style, watching);
